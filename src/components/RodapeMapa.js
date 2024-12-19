566a1cb8d7ef0d2b96012b88c0fd1d4fc0648a1b
heric <heric.moura@unifesp.br>
2024-12-19 12:59:37 -0300

    cartografando

--- src/components/RodapeMapa.js
+++ src/components/RodapeMapa.js
@@ -3,7 +3,7 @@ import React from "react";
 const RodapeMapa = () => (
   <div className="fixed bottom-0 left-0 w-full z-50 p-4 bg-gradient-to-t  text-white text-center">
     <p className="text-sm">
-    Territorialidades, lutas sociais, identidades coletivas e a força dos movimentos sociais de Santos.
+    Territorialidades, lutas, identidades coletivas e a força dos movimentos sociais de Santos.
     </p>
   </div>
 );
