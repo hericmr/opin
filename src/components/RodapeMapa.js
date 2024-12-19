8bd9f76180f851589662e592fa2b4b5b89e9ae2e
heric <heric.moura@unifesp.br>
2024-12-19 12:55:05 -0300

    cartografando

--- src/components/RodapeMapa.js
+++ src/components/RodapeMapa.js
@@ -1,10 +1,9 @@
-// RodapeMapa.js
 import React from "react";
 
 const RodapeMapa = () => (
-  <div className="absolute bottom-0 left-0 w-full z-10 p-4 bg-gradient-to-t from-gray-900 via-gray-800 to-transparent text-white text-center">
+  <div className="fixed bottom-0 left-0 w-full z-50 p-4 bg-gradient-to-t  text-white text-center">
     <p className="text-sm">
-      Explore e conheça mais sobre os pontos destacados da cidade de Santos.
+    Territorialidades, lutas sociais, identidades coletivas e a força dos movimentos sociais de Santos.
     </p>
   </div>
 );
