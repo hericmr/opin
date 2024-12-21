e8996c88856c7fc950014958acde08239b53d798
heric <heric.moura@unifesp.br>
2024-12-21 11:47:15 -0300

    inseri uma descrição do site

--- src/components/MapaSantos.js
+++ src/components/MapaSantos.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import pontos from "./pontosData";
@@ -7,7 +7,30 @@ import PainelInformacoes from "./PainelInformacoes";
 import "./MapaSantos.css";
 
 const MapaSantos = () => {
+  const detalhesIntro = {
+    titulo: "Cartografia Social de Santos",
+    descricao: `
+     <p> Esta plataforma interativa é uma cartografia social que busca mapear as territorialidades e valorizar as lutas e conquistas dos movimentos sociais e da classe trabalhadora na cidade de Santos.</p>
+      <br>
+     <p> O mapa destaca a presença de equipamentos sociais, culturais, religiosos, políticos, educacionais e históricos, como escolas, igrejas, unidades de saúde, espaços culturais e de lazer, além de locais carregados de memória histórica. </p>
+     <br>
+     <p> Entre os elementos mapeados, estão histórias relacionadas à escravização do povo negro, à opressão e resistência à ditadura militar (1964-1984), e às lutas que moldaram e continuam moldando a identidade da região. </p>
+      <br>
+     <p>Os materiais cartográficos e textuais disponíveis aqui foram produzidos pelos estudantes de Serviço Social da UNIFESP, durante a Unidade Curricular de Políticas Públicas 2, em 2024 e 2025.</p>
+    `,
+  };
+
   const [painelInfo, setPainelInfo] = useState(null);
+
+  useEffect(() => {
+    // Temporizador para exibir a mensagem inicial após 15 segundos
+    const timer = setTimeout(() => {
+      setPainelInfo(detalhesIntro);
+    }, 3000);
+
+    // Limpa o temporizador ao desmontar o componente
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleMarkerClick = (detalhes) => {
     setPainelInfo(detalhes);
@@ -21,8 +44,8 @@ const MapaSantos = () => {
     <div className="relative h-screen">
       {/* Mapa */}
       <MapContainer
-        center={[-23.968208, -46.322742]}
-        zoom={13.5}
+        center={[-23.97 , -46.29]}
+        zoom={13}
         className="h-full w-full"
       >
         <TileLayer
