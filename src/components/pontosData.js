70c4dad8e56ccae697eb174b5e5910f7a758956f
heric <heric.moura@unifesp.br>
2025-01-15 18:42:46 -0300

    apaguei a boia verde

--- src/components/pontosData.js
+++ src/components/pontosData.js
@@ -6,10 +6,11 @@ const pontos = [
     detalhes: {
       titulo: "Estátua de Iemanjá",
       descricao: `
-      A estátua de Iemanjá, localizada na Praia do José Menino, é um símbolo cultural de Santos e uma homenagem às tradições afro-brasileiras. 
-      Sua instalação na orla da praia foi uma conquista dos movimentos negros da região, após permanecer guardada no Teatro Municipal de Santos por vários anos.
-    `,
+        A estátua de Iemanjá, localizada na Praia do José Menino, é um símbolo cultural de Santos e uma homenagem às tradições afro-brasileiras. 
+        Sua instalação na orla da praia foi uma conquista dos movimentos negros da região, após permanecer guardada no Teatro Municipal de Santos por vários anos.
+      `,
       video: "https://www.youtube.com/embed/Br_ioeUszH0?si=uZLBcYlnhKm_a1qg",
+      imagens: [], // Adicionado array vazio por padrão
       links: [
         {
           texto: "Levante vai cobrar Estátua de Iemanjá em Santos - DL",
@@ -31,25 +32,11 @@ const pontos = [
       descricao: `
         O Farol do Canal 6 é um ponto icônico de Santos, marcando a entrada do canal e oferecendo uma vista deslumbrante do marzão.
       `,
-      video: null, // Mantido null para pontos sem vídeo
+      video: null, // Vídeo ausente
       imagens: [
         "https://s2.glbimg.com/RfVWirfUe5XAfGdGooaVIg9NQpg=/s.glbimg.com/jo/g1/f/original/2015/01/28/farol12.jpg",
       ],
-      links: [],
-    },
-  },
-  {
-    lat: -23.991275,
-    lng: -46.316918,
-    desc: "Boia Verde",
-    detalhes: {
-      titulo: "Bóia Verde",
-      descricao: `
-        A Bóia Verde é uma referência náutica localizada na entrada do porto de Santos, desempenhando um papel crucial na navegação segura.
-      `,
-      video: "https://www.youtube.com/embed/_dNxyypHnx0",
-      imagens: [],
-      links: [],
+      links: [], // Links mantidos como array vazio
     },
   },
   {
@@ -62,7 +49,8 @@ const pontos = [
         O Teatro Patrícia Galvão é um dos principais espaços culturais de Santos, conhecido por sua arquitetura e por abrigar diversas produções teatrais e eventos culturais ao longo do ano. O teatro leva o nome de Pagu, uma importante feminista, militante de esquerda e artista de Santos.
       `,
       video: "https://www.youtube.com/embed/KvPW7w3DXUE?si=rqqRxqWtjzsyTZ5-",
-      links: [],
+      imagens: [], // Adicionado array vazio para consistência
+      links: [], // Links mantidos como array vazio
     },
   },
 ];
