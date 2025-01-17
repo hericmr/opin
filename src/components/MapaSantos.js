dfb686653d4382e6068c65f28ad06c9437c7b033
heric <heric.moura@unifesp.br>
2025-01-17 17:18:26 -0300

    mudança no painel

--- src/components/MapaSantos.js
+++ src/components/MapaSantos.js
@@ -11,7 +11,7 @@ const MapaSantos = () => {
   const detalhesIntro = {
     titulo: "Cartografia Social de Santos",
     descricao: `
-      <p> Esta é uma cartografia social que busca mapear as territorialidades e valorizar as lutas e conquistas dos movimentos sociais e da classe trabalhadora na cidade de Santos. O mapa destaca a presença de equipamentos sociais, culturais, religiosos, políticos, educacionais e históricos, como escolas, igrejas, unidades de saúde, espaços culturais e de lazer, além de locais carregados de memória histórica. </p>
+      <p> Esta é uma cartografia social que busca mapear as territorialidades e valorizar as lutas e conquistas dos movimentos sociais e da população na cidade de Santos. O mapa destaca a presença de equipamentos sociais, culturais, religiosos, políticos, educacionais e históricos, como escolas, igrejas, unidades de saúde, espaços culturais e de lazer, além de locais carregados de memória histórica. </p>
       <br>
       <p> Entre os elementos mapeados, estão histórias relacionadas à escravização e lutas do povo negro, à opressão e resistência à ditadura empresarial-militar (1964-1984), e às lutas que moldaram e continuam moldando a identidade da região. </p>
       <br>
