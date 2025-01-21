eaa545079347b3b8d8a73df6f981cf8c5d2bb911
heric <heric.moura@unifesp.br>
2025-01-21 09:53:35 -0300

    adicionando o botao serviços

--- src/components/BotaoAlternar.js
+++ src/components/BotaoAlternar.js
@@ -3,7 +3,7 @@ import React from "react";
 const BotaoAlternar = ({ visivel, onClick }) => (
   <button
     onClick={onClick}
-    className="absolute top-20 left-3 z-10 p-2 bg-gray-100 text-black rounded-md shadow"
+    className="absolute top-20 left-3 z-10 p-2 bg-blue-100 text-black rounded-md shadow"
   >
     {visivel ? "Ocultar Bairros" : "Ver Bairros"}
   </button>
---
+++ src/components/pontosAssistencia.js
@@ -0,0 +1,27 @@
+const pontosAssistencia = [
+
+    {
+      lat: -23.93788,
+      lng: -46.319121,
+      desc: "Centro POP Santos",
+      detalhes: {
+        titulo: "Centro POP Santos",
+        descricao: `
+          O Centro de Referência Especializado para População em Situação de Rua (Centro POP) oferece suporte para pessoas em situação de rua,
+          incluindo alimentação, higiene, e encaminhamento para serviços de saúde e assistência social.
+        `,
+        video: "https://www.youtube.com/embed/exampleVideoID", // Link temporario - para esse equipamento é legal o angelo gravar um video
+        imagens: [],
+        links: [
+          {
+            texto: "Centro POP Santos - Prefeitura",
+            url: "https://www.santos.sp.gov.br/centropop",
+          },
+        ],
+      },
+    },
+    
+  ];
+  
+  export default pontosAssistencia;
+  
