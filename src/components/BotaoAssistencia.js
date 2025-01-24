7ed159f7e978c5eb8efd1f780e99a25fc40b548f
heric <heric.moura@unifesp.br>
2025-01-24 18:40:21 -0300

    boatoassistencia

--- src/components/BotaoAssistencia.js
+++ src/components/BotaoAssistencia.js
@@ -3,7 +3,9 @@ import React from "react";
 const BotaoAssistencia = ({ visivel, onClick }) => (
   <button
     onClick={onClick}
-    className="absolute top-32 left-3 z-10 p-2 bg-blue-100 text-black rounded-md shadow"
+    className={`absolute top-32 left-3 z-10 p-2 rounded-md shadow ${
+      visivel ? "bg-green-900 text-white" : "bg-blue-100 text-black"
+    }`}
   >
     {visivel ? "Ocultar Serviços de Assistência" : "Assistência"}
   </button>
