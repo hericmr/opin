3c464813e2d235fc064e2f984d1c957b9061a4f3
heric <heric.moura@unifesp.br>
2025-01-28 19:12:15 -0300

    arrumando local do marcador

--- src/components/CustomIcon.js
+++ src/components/CustomIcon.js
@@ -2,23 +2,23 @@ import L from "leaflet";
 
 // Ícone padrão (vermelho)
 export const customIcon = new L.Icon({
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png", // Ícone padrão do Leaflet
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png", // Ícone de um github para Leaflet
   iconSize: [25, 41], // Tamanho do ícone
-  iconAnchor: [12, 41], // Ponto de ancoragem do ícone
+  iconAnchor: [0, 98], // Ponto de ancoragem do ícone
   popupAnchor: [1, -34], // Posição do popup em relação ao ícone
 });
 
 // Ícone verde
 export const greenIcon = new L.Icon({
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png", // Ícone verde do repositório leaflet-color-markers
-  iconSize: [25, 41], // Tamanho do ícone
-  iconAnchor: [12, 41], // Ponto de ancoragem do ícone
-  popupAnchor: [1, -34], // Posição do popup em relação ao ícone
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png", 
+  iconSize: [25, 41], 
+  iconAnchor: [0, 98], 
+  popupAnchor: [1, -34], 
 });
 // Ícone amarelo
 export const yellowIcon = new L.Icon({
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png", // Ícone amarelo do repositório leaflet-color-markers
-  iconSize: [25, 41], // Tamanho do ícone
-  iconAnchor: [12, 41], // Ponto de ancoragem do ícone
-  popupAnchor: [1, -34], // Posição do popup em relação ao ícone
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png", 
+  iconSize: [25, 41], 
+  iconAnchor: [0, 98], 
+  popupAnchor: [1, -34], 
 });
