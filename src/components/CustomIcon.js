cef77b8ef94eb6ceecd918e38f4912a29ba9d88a
Héric Moura <111160949+hericmr@users.noreply.github.com>
2025-01-27 21:18:05 -0300

    Update CustomIcon.js

--- src/components/CustomIcon.js
+++ src/components/CustomIcon.js
@@ -1,9 +1,17 @@
-// CustomIcon.js
 import L from "leaflet";
 
+// Ícone padrão (vermelho)
 export const customIcon = new L.Icon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-  iconSize: [30, 45],
-  iconAnchor: [15, 45],
-  popupAnchor: [1, -34],
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png", // Ícone padrão do Leaflet
+  iconSize: [25, 41], // Tamanho do ícone
+  iconAnchor: [12, 41], // Ponto de ancoragem do ícone
+  popupAnchor: [1, -34], // Posição do popup em relação ao ícone
+});
+
+// Ícone verde
+export const greenIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png", // Ícone verde do repositório leaflet-color-markers
+  iconSize: [25, 41], // Tamanho do ícone
+  iconAnchor: [12, 41], // Ponto de ancoragem do ícone
+  popupAnchor: [1, -34], // Posição do popup em relação ao ícone
 });
