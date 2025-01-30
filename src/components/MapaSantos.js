dc704548105547f4f66830ddf6a349606a0a19ec
heric <heric.moura@unifesp.br>
2025-01-30 16:10:36 -0300

    adicionando fotodaturma

--- src/components/MapaSantos.js
+++ src/components/MapaSantos.js
@@ -18,7 +18,7 @@ const MapaSantos = () => {
       <p class="mb-4">Esta é uma cartografia social que busca mapear as territorialidades, as lutas e conquistas dos movimentos sociais e da população na cidade de Santos. O mapa destaca a presença de equipamentos sociais, culturais, religiosos, políticos, educacionais, como escolas, unidades de saúde, assistência social, espaços culturais e de lazer, além de locais carregados de memória histórica.</p>
       
       <div class="border border-green-200 p-4 rounded-lg bg-green-100 max-w-md mx-auto">
-        <p class="font-semibold mb-2 text-center">Os pontos estão representados pelas cores:</p>
+        <p class="font-semibold mb-2 text-center">Os pontos estão representados por:</p>
         <ul class="list-none space-y-2">
           <li class="flex items-center">
             <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png" alt="Marcador Azul" class="w-5 h-8 mr-2" />
