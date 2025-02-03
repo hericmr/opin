b53c65ff9a6876fd5e17235bcfa6c2487f8e19a7
heric <heric.moura@unifesp.br>
2025-02-03 12:39:52 -0300

    alterando a detalhes info para um arquivo proprio

--- src/components/MapaSantos.js
+++ src/components/MapaSantos.js
@@ -7,38 +7,10 @@ import PainelInformacoes from "./PainelInformacoes";
 import pontosAssistencia from "./pontosAssistencia";
 import pontosHistoricos from "./pontosHistoricos";
 import pontosLazer from "./pontosLazer";
+import detalhesIntro from "./detalhesInfo"; // Importando os detalhes de introdução
 import "./MapaSantos.css";
 
 const MapaSantos = () => {
-  const detalhesIntro = {
-    titulo: "Territórios, resistência e identidade em Santos",
-    descricao: `
-      <p class="mb-4">Esta é uma cartografia social que busca mapear territorialidades, lutas e conquistas dos movimentos sociais e da população na cidade de Santos. O mapa destaca a presença de equipamentos sociais, culturais, religiosos, políticos, educacionais, como escolas, unidades de saúde, assistência social, espaços culturais e de lazer, além de locais carregados de memória histórica. Os pontos estão representados por:</p>
-      
-      <div class="border border-green-200 p-4 rounded-lg bg-green-100 max-w-lg mx-auto">
-        <ul class="list-none space-y-2">
-          <li class="flex items-center">
-            <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png" alt="Marcador Azul" class="w-5 h-8 mr-2" />
-            <span><strong class="text-blue-700">Lazer:</strong> Equipamentos sociais, culturais e de lazer.</span>
-          </li>
-          <li class="flex items-center">
-            <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png" alt="Marcador Verde" class="w-5 h-8 mr-2" />
-            <span><strong class="text-green-700">Assistência:</strong> Unidades de assistência social e saúde.</span>
-          </li>
-          <li class="flex items-center">
-            <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png" alt="Marcador Amarelo" class="w-5 h-8 mr-2" />
-            <span><strong class="text-yellow-600">Históricos:</strong> Locais históricos e de memória.</span>
-          </li>
-        </ul>
-      </div>
-      
-      <p class="mt-4">Entre os elementos mapeados, estão histórias relacionadas à escravização e lutas do povo negro, à opressão e resistência à ditadura empresarial-militar (1964-1984), e às lutas que moldaram e continuam moldando a identidade da região.</p>
-      
-      <p class="mt-4">Os materiais cartográficos e textuais disponíveis aqui foram produzidos pelas(os) estudantes de Serviço Social da UNIFESP do vespertino e noturno durante a Unidade Curricular de Políticas Públicas 2, em 2024 e 2025.</p>
-      <br><img src="/cartografiasocial/fotos/turma.png" alt="Turma do Vespertino" className="h-10 w-auto object-contain" />
-    `,
-  };
-
   const [painelInfo, setPainelInfo] = useState(detalhesIntro);
   const [geojsonData, setGeojsonData] = useState(null);
   const [visibilidade, setVisibilidade] = useState({
