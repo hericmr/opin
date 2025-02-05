4f45a097ff445e6040c945443aa052b47b453160
heric <heric.moura@unifesp.br>
2025-02-05 08:53:05 -0300

    adicionando urls customizadas por painel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter once
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// This part sets up the root element and renders the application with BrowserRouter
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
