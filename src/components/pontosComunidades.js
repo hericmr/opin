4e57fa0a6894f5bfb9e874f0de86f808312addaf
heric <heric.moura@unifesp.br>
2025-02-06 13:06:01 -0300

    trabulsi

--- src/components/pontosComunidades.js
+++ src/components/pontosComunidades.js
@@ -27,10 +27,31 @@ const pontosComunidades = [
             }
           ]
         }
+      },
+
+      {
+        "lat": -23.9841029,
+        "lng": -46.307967,
+        "desc": "Trabulsi",
+        "tipo": "comunidades",
+        "detalhes": {
+          "titulo": "Trabulsi",
+          "descricao": "A Rua Bassin Nagib Trabulsi, situada na Ponta da Praia, é um espaço de muita circulação e convivência. Fazendo uma mistura de comércio e prédios residenciais, essa via desempenha um papel relevante para a reprodução social da população dessa região, sendo também uma rua de trabalhadores. Entre mercados, adegas, lojinhas de variedades e estabelecimentos de serviços, a rua se destaca pelo movimento constante. Durante o dia, a rua é frequentada por quem busca produtos do cotidiano, enquanto à noite se transforma em um ponto de encontro de diferentes grupos sociais. Nas sextas-feiras, os bares começam a ganhar mais vida, e a rua se torna um ponto de encontro com características pitorescas. Ainda há pessoas que descem dos prédios com cadeiras de praia e se acomodam na calçada, principalmente nos dias e noites de calor, e a movimentação vai até a madrugada. Tem criança chorando, cachorro latindo, bêbado gritando e, no domingo, o tradicional bingo valendo um frango na lanchonete “Wu Jian”, popularmente chamada de \"lanchonete da Chinesa\". E nos finais de ano e durante os carnavais, a “Banda da Trabulsi” toca o maior terror. A rua conserva prédios com apartamentos pequenos, habitados principalmente por moradores idosos que compraram seus apartamentos bem antes de a Ponta da Praia se tornar uma área tão cara, devido à especulação imobiliária. Essa realidade persiste, desafiando os processos de gentrificação intensificados na região e a ideia popular de que a Ponta da Praia é um bairro da classe alta. A Trabulsi, portanto, carrega as marcas da história da Ponta da Praia de Santos e de seus moradores, resistindo às transformações urbanas enquanto mantém vivo seu jeito de ser.",
+          "video": "https://www.youtube.com/embed/i3nnF5DvzWk?si=i5rNU44mgtW731Hz",
+          "imagens": [ "/cartografiasocial/fotos/trabulsi.png"],
+          "links": [
+            {
+              "texto": "Banda da Trabulsi",
+              "url": ""
+            },
+            {
+              "texto": "Primeira fase da revitalização da Rua Trabulsi em Santos termina",
+              "url": "https://www.diariodolitoral.com.br/santos/primeira-fase-da-revitalizacao-da-rua-trabulsi-em-santos-termina/171615/"
+            }
+          ]
+        }
       }
       
-      
-      
   ];
-  
-  export default pontosComunidades; //
+    
+  export default pontosComunidades;
