096c4ae5c7fdc651a259767f52858bfe9051d198
heric <heric.moura@unifesp.br>
2025-02-07 09:45:33 -0300

    removi o mapa do navbar

--- src/components/pontosEducação.js
+++ src/components/pontosEducação.js
@@ -74,9 +74,117 @@ const pontosEducação = [
         }
       ]
     }
+  },
+  {
+    "lat": -23.9793965,
+    "lng": -46.2993718,
+    "desc": "UME Doutor Porchat Assis",
+    "tipo": "educacao",
+    "detalhes": {
+      "titulo": "UME Doutor Porchat Assis",
+      "descricao": "A UME Doutor Porchat Assis, localizada na Ponta da Praia, em Santos, é uma escola municipal que desempenha um papel essencial na formação educacional de crianças e adolescentes da região. \n\nAlém do currículo regular, a UME Doutor Porchat Assis é reconhecida por ter iniciativas comunitárias, como projetos que incentivam a leitura, atividades esportivas e ações culturais que envolvem tanto os estudantes quanto suas famílias. Essas ações reforçam a importância da educação como ferramenta de transformação social.\n\nA escola também enfrenta desafios que refletem as contradições presentes na sociedade brasileira, como a necessidade de ampliação de recursos e investimentos na educação pública. Contudo, a resistência e o empenho de professores, funcionários e da comunidade escolar têm sido fundamentais para superar essas dificuldades e garantir um ensino de qualidade.",
+      "video": null,
+      "imagens": ["/cartografiasocial/fotos/porchat.png"],
+      "links": [
+      ],
+      "contato": {
+        "endereco": "Rua Ana Pimentel, 23, Ponta da Praia, Santos, SP",
+        "telefone": "(13) 3251-5583"
+      }
+    }
+    },
+    {
+      "lat": -23.9780518,
+      "lng": -46.3000682,
+      "desc": "UMEE Profa. Maria Carmelita Proost Vilaça",
+      "tipo": "educação",
+      "detalhes": {
+          "titulo": "UMEE Profa. Maria Carmelita Proost Vilaça",
+          "descricao": "A UME Professora Maria Carmelita Proost Villaça, localizada na Ponta da Praia, em Santos, é uma instituição dedicada ao atendimento educacional de crianças e adolescentes com deficiências. Fundada em 1960, a escola tem se consolidado como um espaço de inclusão e aprendizado para alunos com necessidades especiais. A escola oferece atendimento educacional especializado, contando com salas de recursos multifuncionais e profissionais qualificados para atender às diversas necessidades dos alunos. Além disso, a infraestrutura da escola é adaptada para garantir acessibilidade, incluindo banheiros adequados para pessoas com deficiência. Em 2019, a Secretaria de Educação de Santos iniciou estudos para transformar a UME Carmelita em uma unidade de ensino integral, ampliando sua capacidade para 400 estudantes. Essa proposta visava integrar alunos com e sem deficiência em um ambiente educacional inclusivo. No entanto, essa iniciativa gerou preocupações entre os pais e a comunidade escolar, que temiam a redução do atendimento especializado para alunos com deficiência. A comunidade escolar, incluindo pais e educadores, tem se mobilizado para garantir que a UME Carmelita continue a oferecer um atendimento especializado de qualidade para alunos com deficiência. Movimentos como 'Somos Todos Carmelita' têm sido fundamentais na defesa da manutenção e aprimoramento dos serviços prestados pela escola.",
+          "video": null,
+          "imagens": [ "/cartografiasocial/fotos/carmelita.png",
+          "/cartografiasocial/fotos/carmelita2.png" ],
+          "links": [
+              {
+                  "texto": "Santos SP - UME Carmelita",
+                  "url": "https://www.santos.sp.gov.br/?q=local%2Fume-professora-maria-carmelita-proost-villaca"
+              },
+              {
+                  "texto": "Santos SP - Projeto de ensino integral",
+                  "url": "https://g1.globo.com/sp/santos-regiao/noticia/2019/03/26/seduc-estuda-periodo-integral-para-400-criancas-em-escola-de-ensino-especial-de-santos-sp.ghtml"
+              }
+          ]
+      }  
+    },
+   //{
+  /*
+    "lat": -23.973561,
+    "lng": -46.325978,
+    "desc": "Escola Municipal de Educação Profa. Maria Luíza Alonso Silva",
+    "tipo": "educação",
+    "detalhes": {
+      "titulo": "Escola Municipal de Educação Profa. Maria Luíza Alonso Silva",
+      "descricao": "Escola municipal de ensino fundamental, localizada na Ponta da Praia.",
+      "video": null,
+      "imagens": [],
+      "links": []
+    }
+      },
+  {
+    "lat": -23.975489,
+      "lng": -46.320769,
+        "desc": "Escola Municipal Profa. Iveta Mesquita Nogueira",
+          "tipo": "educação",
+            "detalhes": {
+      "titulo": "Escola Municipal Profa. Iveta Mesquita Nogueira",
+        "descricao": "Escola municipal localizada na Ponta da Praia.",
+          "video": null,
+            "imagens": [],
+              "links": []
+    }
+  },
+  {
+    "lat": -23.973612,
+      "lng": -46.322034,
+        "desc": "Unidade Municipal de Ensino Pedro II",
+          "tipo": "educação",
+            "detalhes": {
+      "titulo": "Unidade Municipal de Ensino Pedro II",
+        "descricao": "Escola municipal localizada na Ponta da Praia.",
+          "video": null,
+            "imagens": [],
+              "links": []
+    }
+  },
+  {
+    "lat": -23.973951,
+      "lng": -46.321561,
+        "desc": "Escola Estadual Dona Luiza Macuco",
+          "tipo": "educação",
+            "detalhes": {
+      "titulo": "Escola Estadual Dona Luiza Macuco",
+        "descricao": "Escola estadual localizada na Ponta da Praia.",
+          "video": null,
+            "imagens": [],
+              "links": []
+    }
+  },
+  {
+    "lat": -23.974530,
+      "lng": -46.325734,
+        "desc": "ETEC Aristóteles Ferreira",
+          "tipo": "educação",
+            "detalhes": {
+      "titulo": "ETEC Aristóteles Ferreira",
+        "descricao": "Escola técnica localizada na Aparecida, próxima à Ponta da Praia.",
+          "video": null,
+            "imagens": [],
+              "links": []
+    }
   }
-  
-  
+  */
+
+
 
 ];
 
