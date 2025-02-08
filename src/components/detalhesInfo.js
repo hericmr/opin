044aa2f37d961e746be7be48343c1cd34a3c625d
heric <heric.moura@unifesp.br>
2025-02-07 22:08:44 -0300

    arrumei o texto conforme a fala da let

--- src/components/detalhesInfo.js
+++ src/components/detalhesInfo.js
@@ -35,7 +35,7 @@ const detalhesIntro = {
           
           <p class="mt-4">Entre os elementos mapeados, estão histórias relacionadas à escravidão e lutas do povo negro, à opressão e resistência à ditadura empresarial-militar (1964-1984), e às lutas que moldaram e continuam moldando a identidade da região.</p>
           
-          <p class="mt-4">Os materiais cartográficos e textuais disponíveis aqui foram produzidos pelas(os) estudantes de Serviço Social da UNIFESP do vespertino e noturno durante a Unidade Curricular de Políticas Públicas 2, em 2024 e 2025.</p>
+          <p class="mt-4">Os materiais cartográficos e textuais disponíveis aqui foram produzidos pelas(os) estudantes de Serviço Social da UNIFESP do vespertino e noturno durante a Unidade Curricular de Política Social 2, em 2024 e 2025.</p>
           <br><img src="/cartografiasocial/fotos/turma.png" alt="Turma do Vespertino" className="h-10 w-auto object-contain" />
         `,
 };
