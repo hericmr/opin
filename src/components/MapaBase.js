6eb84ce9a82912580b72c5363d7e315c8536181d
hericmr <seu-email@dominio.com>
2025-02-10 10:39:07 -0300

    arrumando mapas no cel

--- src/components/MapaBase.js
+++ src/components/MapaBase.js
@@ -7,7 +7,7 @@ const isMobile = () => {
 
 const MapaBase = ({ children }) => {
   // Define coordenadas e zoom baseados no dispositivo
-  const defaultPosition = isMobile() ? [-23.96, -46.36] : [-23.97, -46.29];
+  const defaultPosition = isMobile() ? [-23.98, -46.36] : [-23.97, -46.29];
   const defaultZoom = isMobile() ? 12 : 13; // Ajuste do zoom para mobile
 
   return (
