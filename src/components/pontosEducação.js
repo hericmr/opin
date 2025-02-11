2493b1b624c1252a1f2784629eb604e1c3c4d319
hericmr <heric.moura@unifesp.br>
2025-02-11 17:14:41 -0300

    adicionando dados ceeja maria pasqualeto

--- src/components/pontosEducação.js
+++ src/components/pontosEducação.js
@@ -1,6 +1,5 @@
 const pontosEducação = [
-
-{
+  {
     "lat": -23.9531346,
     "lng": -46.3403866,
     "desc": "E. E. Prof. Primo Ferreira",
@@ -9,8 +8,7 @@ const pontosEducação = [
       "titulo": "E. E. Prof. Primo Ferreira",
       "descricao": "Escola pública estadual em Santos, SP, oferecendo ensino fundamental, médio e EJA.",
       "video": null,
-      "imagens": [ "/cartografiasocial/fotos/primo.jpeg",
-        "/cartografiasocial/fotos/primo2.png" ],
+      "imagens": ["/cartografiasocial/fotos/primo.jpeg", "/cartografiasocial/fotos/primo2.png"],
       "links": [
         {
           "texto": "Blog da E. E. Prof. Primo Ferreira",
@@ -36,7 +34,7 @@ const pontosEducação = [
       "titulo": "E. E. Azevedo Junior",
       "descricao": "Escola pública estadual em Santos, SP, oferecendo ensino fundamental e médio.",
       "video": null,
-      "imagens": [ "/cartografiasocial/fotos/azevedo.jpeg"],
+      "imagens": ["/cartografiasocial/fotos/azevedo.jpeg"],
       "links": [
         {
           "texto": "Detalhes da escola no site da Secretaria da Educação",
@@ -62,7 +60,7 @@ const pontosEducação = [
       "titulo": "Colégio Lamec",
       "descricao": "Escola particular em Santos, SP, oferecendo Educação Infantil e Ensino Fundamental.",
       "video": null,
-      "imagens": [ "/cartografiasocial/fotos/lamec.jpeg"],
+      "imagens": ["/cartografiasocial/fotos/lamec.jpeg"],
       "links": [
         {
           "texto": "Perfil no Instagram",
@@ -85,202 +83,125 @@ const pontosEducação = [
       "descricao": "A UME Doutor Porchat Assis, localizada na Ponta da Praia, em Santos, é uma escola municipal que desempenha um papel essencial na formação educacional de crianças e adolescentes da região. \n\nAlém do currículo regular, a UME Doutor Porchat Assis é reconhecida por ter iniciativas comunitárias, como projetos que incentivam a leitura, atividades esportivas e ações culturais que envolvem tanto os estudantes quanto suas famílias. Essas ações reforçam a importância da educação como ferramenta de transformação social.\n\nA escola também enfrenta desafios que refletem as contradições presentes na sociedade brasileira, como a necessidade de ampliação de recursos e investimentos na educação pública. Contudo, a resistência e o empenho de professores, funcionários e da comunidade escolar têm sido fundamentais para superar essas dificuldades e garantir um ensino de qualidade.",
       "video": null,
       "imagens": ["/cartografiasocial/fotos/porchat.png"],
-      "links": [
-      ],
+      "links": [],
       "contato": {
         "endereco": "Rua Ana Pimentel, 23, Ponta da Praia, Santos, SP",
         "telefone": "(13) 3251-5583"
       }
     }
-    },
-    {
-      "lat": -23.9780518,
-      "lng": -46.3000682,
-      "desc": "UMEE Profa. Maria Carmelita Proost Vilaça",
-      "tipo": "educação",
-      "detalhes": {
-          "titulo": "UMEE Profa. Maria Carmelita Proost Vilaça",
-          "descricao": "A UME Professora Maria Carmelita Proost Villaça é um colégio localizado na Ponta da Praia, em Santos, que atende crianças e adolescentes com deficiências. A escola é uma das mais antigas instituições de educação especial da cidade e foi fundada em 1960. Seu propósito é proporcionar inclusão e aprendizado para alunos com necessidades especiais. Nesse sentido, a escola dispõe de atendimento educacional especializado, possui salas de recursos multifuncionais e profissionais capacitados para atender a necessidades específicas. A estrutura do colégio também é adequada à acessibilidade com banheiros, inclusive para pessoas com deficiência. A escola recebeu apoio da Secretaria de Educação de Santos, em 2019, que iniciou estudos para torná-la uma UME de ensino integral, capacidade para 400 alunos. O projeto foi desenhado para juntar alunos com e sem deficiências em um ambiente educacional inclusivo. No entanto, essa iniciativa gerou preocupações entre os pais e a comunidade escolar, que temiam a redução do atendimento especializado para alunos com deficiência. A comunidade escolar, incluindo pais e educadores, tem se mobilizado para garantir que a UME Carmelita continue a oferecer um atendimento especializado de qualidade para alunos com deficiência. Movimentos como 'Somos Todos Carmelita' têm sido fundamentais na defesa da manutenção e aprimoramento dos serviços prestados pela escola.\n\n**Endereço:** Av. Prof. Aristóteles Menezes, 11 - Ponta da Praia, Santos - SP.\n\n- **Telefone:** 01332369997.",
-          "video": null,
-          "imagens": [ "/cartografiasocial/fotos/carmelita.png",
-          "/cartografiasocial/fotos/carmelita2.png" ],
-          "links": [
-              {
-                  "texto": "Santos SP - UME Carmelita",
-                  "url": "https://www.santos.sp.gov.br/?q=local%2Fume-professora-maria-carmelita-proost-villaca"
-              },
-              {
-                  "texto": "Santos SP - Projeto de ensino integral",
-                  "url": "https://g1.globo.com/sp/santos-regiao/noticia/2019/03/26/seduc-estuda-periodo-integral-para-400-criancas-em-escola-de-ensino-especial-de-santos-sp.ghtml"
-              },
-              {
-                  "texto": "Mães se reúnem para evitar o fim de escola especial para crianças com deficiência",
-                  "url": "https://g1.globo.com/sp/santos-regiao/noticia/2019/06/04/maes-se-reunem-para-evitar-o-fim-de-escola-especial-para-criancas-com-deficiencia.ghtml"
-                                }
-          ]
-      }  
-    },
-    {
-      "lat": -23.9808059,
-      "lng": -46.3118476,
-      "desc": "E. E. Antônio Ablas Filho Dr",
-      "tipo": "educação",
-      "detalhes": {
-        "titulo": "E. E. Antônio Ablas Filho Dr",
-        "descricao": "Escola estadual, em Santos, SP.",}
-    },
-    {
-      "lat": -23.9529676,
-      "lng": -46.3403866, // Ajuste a longitude conforme necessário
-      "desc": "UME Professora Emília Maria Reis",
-      "tipo": "educação",
-      "detalhes": {
-        "titulo": "UME Professora Emília Maria Reis",
-        "descricao": "Escola municipal em Santos, SP, oferecendo ensino infantil e fundamental.",
-        "video": null,
-        "imagens": [],
-        "links": [
-          {
-            "texto": "Localização no Google Maps",
-            "url": "https://maps.app.goo.gl/JRoFW7tCuDcH9dkB8"
-          },
-          {
-            "lat": -23.9535718,
-            "lng": -46.3412044,
-            "desc": "Praça Olímpio de Lima",
-            "tipo": "lazer",
-            "detalhes": {
-              "titulo": "Praça Olímpio de Lima",
-              "descricao": "Praça pública localizada em Santos, SP, ideal para passeios, atividades ao ar livre e momentos de lazer.",
-              "video": null,
-              "imagens": [],
-              "links": [
-                {
-                  "texto": "Localização no Google Maps",
-                  "url": "https://maps.app.goo.gl/5HGdUyVBp6faEiKU8?g_st=ac"
-                }
-              ]
-            }
-          },
-
-
-{
-  "lat": -23.9794762,
-  "lng": -46.3143102,
-  "desc": "Biblioteca Municipal \"Mário Faria\"",
-  "tipo": "educação",
-  "detalhes": {
-    "titulo": "Biblioteca Municipal \"Mário Faria\"",
-    "descricao": "Biblioteca pública localizada na orla da praia de Santos, SP, oferecendo um acervo variado para empréstimo e um espaço aconchegante para leitura à beira-mar.",
-    "video": null,
-    "imagens": [],
-    "links": [
-      {
-        "texto": "Localização no Google Maps",
-        "url": "https://www.google.com/maps/place/Biblioteca+Municipal+%22M%C3%A1rio+Faria%22/@-23.9794524,-46.314392,25m/data=!3m1!1e3!4m10!1m3!11m2!2s-1-j9UfOTqu3-JRdLa1M-g!3e3!3m5!1s0x94ce02454a3ec351:0xac4b9164cfac4aa4!8m2!3d-23.9794762!4d-46.3143102!16s%2Fg%2F1q67djm_y?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D"
-      }
-    ]
-  }
-},
-
-{
-  "lat": -23.9808059,
-  "lng": -46.3143102, // Ajuste a longitude conforme necessário
-  "desc": "CEEJA Maria Aparecida Pasqualeto Figueiredo",
-  "tipo": "educação",
-  "detalhes": {
-    "titulo": "CEEJA Maria Aparecida Pasqualeto Figueiredo",
-    "descricao": "Centro Estadual de Educação de Jovens e Adultos (CEEJA) localizado na Av. Dr. Epitácio Pessoa, 432 - Aparecida, Santos - SP, 11030-600. Oferece oportunidades de ensino para jovens e adultos. Horário de funcionamento: Aberto até às 22:00.",
-    "video": null,
-    "imagens": [],
-    "links": [
-      {
-        "texto": "Site oficial",
-        "url": "http://ceejamar.com.br"
-      },
-      {
-        "texto": "Localização no Google Maps",
-        "url": "https://www.google.com/maps/place/CEEJA+MARIA+APARECIDA+PASQUALETO+FIGUEIREDO/@-23.9808059,-46.3143102,25m/data=!3m1!1e3!4m10!1m3!11m2!2s-1-j9UfOTqu3-JRdLa1M-g!3e3!3m5!1s0x94ce02454a3ec351:0xac4b9164cfac4aa4!8m2!3d-23.9794762!4d-46.3143102!16s%2Fg%2F1q67djm_y?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D"
-      }
-    ]
-  }
-}
-          
-
-        ]
-      }
-    }
-
-   //{
-  /*
-    "lat": -23.973561,
-    "lng": -46.325978,
-    "desc": "Escola Municipal de Educação Profa. Maria Luíza Alonso Silva",
+  },
+  {
+    "lat": -23.9780518,
+    "lng": -46.3000682,
+    "desc": "UMEE Profa. Maria Carmelita Proost Vilaça",
     "tipo": "educação",
     "detalhes": {
-      "titulo": "Escola Municipal de Educação Profa. Maria Luíza Alonso Silva",
-      "descricao": "Escola municipal de ensino fundamental, localizada na Ponta da Praia.",
+      "titulo": "UMEE Profa. Maria Carmelita Proost Vilaça",
+      "descricao": "A UME Professora Maria Carmelita Proost Villaça é um colégio localizado na Ponta da Praia, em Santos, que atende crianças e adolescentes com deficiências. A escola é uma das mais antigas instituições de educação especial da cidade e foi fundada em 1960. Seu propósito é proporcionar inclusão e aprendizado para alunos com necessidades especiais. Nesse sentido, a escola dispõe de atendimento educacional especializado, possui salas de recursos multifuncionais e profissionais capacitados para atender a necessidades específicas. A estrutura do colégio também é adequada à acessibilidade com banheiros, inclusive para pessoas com deficiência. A escola recebeu apoio da Secretaria de Educação de Santos, em 2019, que iniciou estudos para torná-la uma UME de ensino integral, capacidade para 400 alunos. O projeto foi desenhado para juntar alunos com e sem deficiências em um ambiente educacional inclusivo. No entanto, essa iniciativa gerou preocupações entre os pais e a comunidade escolar, que temiam a redução do atendimento especializado para alunos com deficiência. A comunidade escolar, incluindo pais e educadores, tem se mobilizado para garantir que a UME Carmelita continue a oferecer um atendimento especializado de qualidade para alunos com deficiência. Movimentos como 'Somos Todos Carmelita' têm sido fundamentais na defesa da manutenção e aprimoramento dos serviços prestados pela escola.\n\n**Endereço:** Av. Prof. Aristóteles Menezes, 11 - Ponta da Praia, Santos - SP.\n\n- **Telefone:** 01332369997.",
+      "video": null,
+      "imagens": ["/cartografiasocial/fotos/carmelita.png", "/cartografiasocial/fotos/carmelita2.png"],
+      "links": [
+        {
+          "texto": "Santos SP - UME Carmelita",
+          "url": "https://www.santos.sp.gov.br/?q=local%2Fume-professora-maria-carmelita-proost-villaca"
+        },
+        {
+          "texto": "Santos SP - Projeto de ensino integral",
+          "url": "https://g1.globo.com/sp/santos-regiao/noticia/2019/03/26/seduc-estuda-periodo-integral-para-400-criancas-em-escola-de-ensino-especial-de-santos-sp.ghtml"
+        },
+        {
+          "texto": "Mães se reúnem para evitar o fim de escola especial para crianças com deficiência",
+          "url": "https://g1.globo.com/sp/santos-regiao/noticia/2019/06/04/maes-se-reunem-para-evitar-o-fim-de-escola-especial-para-criancas-com-deficiencia.ghtml"
+        }
+      ]
+    }
+  },
+  {
+    "lat": -23.9808059,
+    "lng": -46.3118476,
+    "desc": "E. E. Antônio Ablas Filho Dr",
+    "tipo": "educação",
+    "detalhes": {
+      "titulo": "E. E. Antônio Ablas Filho Dr",
+      "descricao": "Escola estadual, em Santos, SP."
+    }
+  },
+  {
+    "lat": -23.9529676,
+    "lng": -46.3403866,
+    "desc": "UME Professora Emília Maria Reis",
+    "tipo": "educação",
+    "detalhes": {
+      "titulo": "UME Professora Emília Maria Reis",
+      "descricao": "Escola municipal em Santos, SP, oferecendo ensino infantil e fundamental.",
       "video": null,
       "imagens": [],
-      "links": []
-    }
-      },
-  {
-    "lat": -23.975489,
-      "lng": -46.320769,
-        "desc": "Escola Municipal Profa. Iveta Mesquita Nogueira",
-          "tipo": "educação",
-            "detalhes": {
-      "titulo": "Escola Municipal Profa. Iveta Mesquita Nogueira",
-        "descricao": "Escola municipal localizada na Ponta da Praia.",
-          "video": null,
-            "imagens": [],
-              "links": []
+      "links": [
+        {
+          "texto": "Localização no Google Maps",
+          "url": "https://maps.app.goo.gl/JRoFW7tCuDcH9dkB8"
+        }
+      ]
     }
   },
   {
-    "lat": -23.973612,
-      "lng": -46.322034,
-        "desc": "Unidade Municipal de Ensino Pedro II",
-          "tipo": "educação",
-            "detalhes": {
-      "titulo": "Unidade Municipal de Ensino Pedro II",
-        "descricao": "Escola municipal localizada na Ponta da Praia.",
-          "video": null,
-            "imagens": [],
-              "links": []
+    "lat": -23.9535718,
+    "lng": -46.3412044,
+    "desc": "Praça Olímpio de Lima",
+    "tipo": "lazer",
+    "detalhes": {
+      "titulo": "Praça Olímpio de Lima",
+      "descricao": "Praça pública localizada em Santos, SP, ideal para passeios, atividades ao ar livre e momentos de lazer.",
+      "video": null,
+      "imagens": [],
+      "links": [
+        {
+          "texto": "Localização no Google Maps",
+          "url": "https://maps.app.goo.gl/5HGdUyVBp6faEiKU8?g_st=ac"
+        }
+      ]
     }
   },
   {
-    "lat": -23.973951,
-      "lng": -46.321561,
-        "desc": "Escola Estadual Dona Luiza Macuco",
-          "tipo": "educação",
-            "detalhes": {
-      "titulo": "Escola Estadual Dona Luiza Macuco",
-        "descricao": "Escola estadual localizada na Ponta da Praia.",
-          "video": null,
-            "imagens": [],
-              "links": []
+    "lat": -23.9794762,
+    "lng": -46.3143102,
+    "desc": "Biblioteca Municipal \"Mário Faria\"",
+    "tipo": "cultura",
+    "detalhes": {
+      "titulo": "Biblioteca Municipal \"Mário Faria\"",
+      "descricao": "Biblioteca pública localizada na orla da praia de Santos, SP, oferecendo um acervo variado para empréstimo e um espaço aconchegante para leitura à beira-mar.",
+      "video": null,
+      "imagens": [],
+      "links": [
+        {
+          "texto": "Localização no Google Maps",
+          "url": "https://www.google.com/maps/place/Biblioteca+Municipal+%22M%C3%A1rio+Faria%22/@-23.9794524,-46.314392,25m/data=!3m1!1e3!4m10!1m3!11m2!2s-1-j9UfOTqu3-JRdLa1M-g!3e3!3m5!1s0x94ce02454a3ec351:0xac4b9164cfac4aa4!8m2!3d-23.9794762!4d-46.3143102!16s%2Fg%2F1q67djm_y?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D"
+        }
+      ]
     }
   },
   {
-    "lat": -23.974530,
-      "lng": -46.325734,
-        "desc": "ETEC Aristóteles Ferreira",
-          "tipo": "educação",
-            "detalhes": {
-      "titulo": "ETEC Aristóteles Ferreira",
-        "descricao": "Escola técnica localizada na Aparecida, próxima à Ponta da Praia.",
-          "video": null,
-            "imagens": [],
-              "links": []
+    "lat": -23.9792099,
+    "lng": -46.3104148,
+    "desc": "CEEJA Maria Aparecida Pasqualeto Figueiredo",
+    "tipo": "educação",
+    "detalhes": {
+      "titulo": "CEEJA Maria Aparecida Pasqualeto Figueiredo",
+      "descricao": "Centro Estadual de Educação de Jovens e Adultos (CEEJA) localizado na Av. Dr. Epitácio Pessoa, 432 - Aparecida, Santos - SP, 11030-600. Oferece oportunidades de ensino para jovens e adultos. Horário de funcionamento: Aberto até às 22:00.",
+      "video": null,
+      "imagens": [],
+      "links": [
+        {
+          "texto": "Site oficial",
+          "url": "http://ceejamar.com.br"
+        },
+        {
+          "texto": "Localização no Google Maps",
+          "url": "https://www.google.com/maps/place/CEEJA+MARIA+APARECIDA+PASQUALETO+FIGUEIREDO/@-23.9808059,-46.3143102,25m/data=!3m1!1e3!4m10!1m3!11m2!2s-1-j9UfOTqu3-JRdLa1M-g!3e3!3m5!1s0x94ce02454a3ec351:0xac4b9164cfac4aa4!8m2!3d-23.9794762!4d-46.3143102!16s%2Fg%2F1q67djm_y?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D"
+        }
+      ]
     }
   }
-  */
-
-
-  
 ];
 
 export default pontosEducação;
