354c1612cf43200e668ac732698a0b492d080626
hericmr <seu-email@dominio.com>
2025-02-17 10:10:15 -0300

    adicionando firula no carregamento

--- src/App.js
+++ src/App.js
@@ -5,12 +5,29 @@ import MapaSantos from "./components/MapaSantos";
 import Navbar from "./components/Navbar"; 
 import PainelInformacoes from "./components/PainelInformacoes";
 
-//carrega os dados da planilha
 const fetchDataPoints = async () => {
   const spreadsheetId = '10h3GnQFWcHa8gZL1YfkJyMjxV3j24z1H-RtqU7TReYY'
   const parser = new PublicGoogleSheetsParser(spreadsheetId)
   return await parser.parse();
 };
+const LoadingScreen = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-900 text-white">
+      <div className="relative">
+        {/* Spinner Animado */}
+        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <img src="/cartografiasocial/favicon.ico" alt="Ícone de carregamento" className="w-8 h-8" />
+        </div>
+      </div>
+
+      <p className="mt-4 text-lg font-semibold">Carregando dados...</p>
+    </div>
+  );
+};
+
 
 const App = () => {
   const [dataPoints, setDataPoints] = useState(null);
@@ -24,51 +41,33 @@ const App = () => {
         dataPoints = formatData(dataPoints);
         setDataPoints(dataPoints);
       } catch (err) {
-          setError(err.message);
+        setError(err.message);
       } finally {
-          setLoading(false);
-      }
-
-      //estrutura os campos de links e imagens para facilitar seu consumo
-      function formatData(dataPoints) {
-        dataPoints = dataPoints.map((e) => {
-          if (e.links) {
-            e.links = e.links.split(";").map((l) => {
-              let linkResult = { texto: "", url: "" };
-              let linkFields = l.split(':');
-              if (linkFields.length > 1) {
-                linkResult.texto = linkFields[0];
-                linkResult.url = linkFields[1];
-              } else {
-                linkResult.url = linkFields[0];
-              }
-              return linkResult;
-            });
-          } else {
-            e.links = [];
-          }
-
-          if (e.imagens) {
-            e.imagens = e.imagens.split(",");
-          } else {
-            e.imagens = [];
-          }
-
-          return e;
-        });
-        return dataPoints;
+        setLoading(false);
       }
     };
+
+    function formatData(dataPoints) {
+      return dataPoints.map((e) => {
+        e.links = e.links ? e.links.split(";").map((l) => {
+          let [texto, url] = l.split(':');
+          return { texto: texto || "", url };
+        }) : [];
+
+        e.imagens = e.imagens ? e.imagens.split(",") : [];
+        return e;
+      });
+    }
 
     initializeApp();
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingScreen />;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <div className="text-center text-red-500 mt-10">Erro: {error}</div>;
   }
 
   return (
