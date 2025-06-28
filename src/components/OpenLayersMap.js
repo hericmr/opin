28ab60b5b5a79dac68de0fe77e468a1b16387017
Héric Moura <heric.m.r@gmail.com>
2025-06-27 23:28:05 -0300

    Ajustando posicionamento do elemento de informações do mapa no mobile

--- src/components/OpenLayersMap.js
+++ src/components/OpenLayersMap.js
@@ -1016,7 +1016,7 @@ const OpenLayersMap = ({
   return (
     <div className={className} ref={mapContainer}>
       {/* Informações do mapa */}
-      <div className="absolute bottom-4 left-4 z-10 bg-white bg-opacity-95 rounded-lg shadow-lg p-3">
+      <div className="absolute bottom-20 sm:bottom-4 left-4 z-10 bg-white bg-opacity-95 rounded-lg shadow-lg p-3">
         <div className="text-xs text-gray-600">
           Lat: {mapInfo.lat} | Lng: {mapInfo.lng}
         </div>
