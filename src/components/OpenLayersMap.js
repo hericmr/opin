eced95b09f122f2dafd3420be75920106d523413
Héric Moura <heric.m.r@gmail.com>
2025-07-04 20:21:31 -0300

    refactor: remover bloco de informações de latitude, longitude e zoom do mapa

--- src/components/OpenLayersMap.js
+++ src/components/OpenLayersMap.js
@@ -670,14 +670,7 @@ const OpenLayersMap = ({
   return (
     <MapWrapper ref={mapContainer}>
       {/* Informações do mapa */}
-      <div className="absolute bottom-20 sm:bottom-4 left-4 z-10 bg-white bg-opacity-95 rounded-lg shadow-lg p-3">
-        <div className="text-xs text-gray-600">
-          Lat: {mapInfo.lat} | Lng: {mapInfo.lng}
-        </div>
-        <div className="text-xs text-gray-600">
-          Zoom: {mapInfo.zoom}
-        </div>
-      </div>
+      {/* Bloco removido conforme solicitado */}
     </MapWrapper>
   );
 };
