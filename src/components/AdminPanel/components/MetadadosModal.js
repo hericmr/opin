b7f2ae56ce98cb81bc9a7f9bf49d79ce177a8eac
hericmr <hericmr@example.com>
2025-11-08 22:52:26 -0300

    chore: remove import não utilizado (CheckCircle)

--- src/components/AdminPanel/components/MetadadosModal.js
+++ src/components/AdminPanel/components/MetadadosModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Info, AlertCircle, CheckCircle, X } from 'lucide-react';
+import { Info, AlertCircle, X } from 'lucide-react';
 import { VersionamentoService } from '../../../services/versionamentoService';
 
 const MetadadosModal = ({ 
